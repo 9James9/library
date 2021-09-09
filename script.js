a42a6c0777748679d65802cd1584018f618ef3d2
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-09 18:07:27 -0500

    clicking add book now displays the book info on the page

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-
-
-let button = document.getElementById('addbook')
-
 function Book(title,author,pages,read) {
     this.title = title
     this.author = author
@@ -14,13 +8,17 @@ let myLibrary = []
 function addBookToLibrary() {
     let title = document.getElementById('titleinput').value
     let author = document.getElementById('authorinput').value
-    let pages = document.getElementById('pagesinput').value
-    
+    let pages = document.getElementById('pagesinput').value  
     let read = document.getElementById('radioform')
     read = (read.elements["read"].value)
     myLibrary.push(new Book(title,author,pages,read))
-    
+    console.log(myLibrary)
+    let bookContainer = document.querySelector('#newbooks')
+    bookContainer.textContent += (`${title},${author},${pages},${read}`)
+
+   
 }
+let button = document.getElementById('addbook')
 button.addEventListener('click', () => {
     addBookToLibrary()
 })
