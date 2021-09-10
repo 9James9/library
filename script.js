db83f8803b785fd76d7b7a07c6f31ba1e8be3603
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-10 10:34:10 -0500

    added books are now displayed on the screen

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ class Book {
 }
 
 let myLibrary = []
-myLibrary.push(new Book('this is the title','author',69,false))
-myLibrary.push(new Book('second book','author',69,false))
+//myLibrary.push(new Book('this is the title','author',69,false))
+//myLibrary.push(new Book('second book','author',69,false))
 
-for (let i = 0; i < myLibrary.length; i++){
-    console.log(myLibrary[i].title)
-}
+
 
 function addBookToLibrary() {
     let title = document.getElementById('titleinput').value
@@ -24,13 +22,18 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title,author,pages,read))
     console.log(myLibrary)
     let bookContainer = document.querySelector('#newbooks')
-    bookContainer.textContent = (`${title},${author},${pages},${read}`)
+    //bookContainer.textContent = (`${title},${author},${pages},${read}`)
+    //loop through myLibrary and display each book
+    for (let i = 0; i < myLibrary.length; i++){
+        
+        let m = document.createElement('div')
+        bookContainer.appendChild(m)
+        m.textContent = `Title: ${myLibrary[i].title} Author: ${myLibrary[i].author} Pages: ${myLibrary[i].pages} Read: ${myLibrary[i].read}`
+    }
     
-    //let newBook = document.createElement('div')
-    //bookContainer.appendChild(newBook)
-    //bookContainer.textContent = ''
     
 }
+
 let button = document.getElementById('addbook')
 button.addEventListener('click', () => {
     addBookToLibrary()
