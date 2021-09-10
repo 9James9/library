2bd4f7f1e171afa426db257d54521c8d30138a56
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-10 10:22:53 -0500

    added for loop to console log each book in myLibrary

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function Book(title,author,pages,read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
+
 let myLibrary = []
+myLibrary.push(new Book('this is the title','author',69,false))
+myLibrary.push(new Book('second book','author',69,false))
+
+for (let i = 0; i < myLibrary.length; i++){
+    console.log(myLibrary[i].title)
+}
+
 function addBookToLibrary() {
     let title = document.getElementById('titleinput').value
     let author = document.getElementById('authorinput').value
@@ -14,9 +24,12 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title,author,pages,read))
     console.log(myLibrary)
     let bookContainer = document.querySelector('#newbooks')
-    bookContainer.textContent += (`${title},${author},${pages},${read}`)
-
-   
+    bookContainer.textContent = (`${title},${author},${pages},${read}`)
+    
+    //let newBook = document.createElement('div')
+    //bookContainer.appendChild(newBook)
+    //bookContainer.textContent = ''
+    
 }
 let button = document.getElementById('addbook')
 button.addEventListener('click', () => {
