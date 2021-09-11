bafca92ceab786af6eb755f9a5da09db3b09675e
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-11 16:13:08 -0500

    inputs are now displayed on screen without duplicates

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ class Book {
         this.pages = pages
         this.read = read
     }
+    info(){
+        return `${this.title} by ${this.author}, pages: ${this.pages}, read? ${this.read}`
+    }
 }
 
 let myLibrary = []
-//myLibrary.push(new Book('this is the title','author',69,false))
-//myLibrary.push(new Book('second book','author',69,false))
-
-
+myLibrary.push(new Book('this is the title','author1',69,false))
+myLibrary.push(new Book('second book','author2',420,true))
 
 function addBookToLibrary() {
     let title = document.getElementById('titleinput').value
@@ -20,18 +21,8 @@ function addBookToLibrary() {
     let read = document.getElementById('radioform')
     read = (read.elements["read"].value)
     myLibrary.push(new Book(title,author,pages,read))
-    console.log(myLibrary)
-    let bookContainer = document.querySelector('#newbooks')
-    //bookContainer.textContent = (`${title},${author},${pages},${read}`)
-    //loop through myLibrary and display each book
-    for (let i = 0; i < myLibrary.length; i++){
-        
-        let m = document.createElement('div')
-        bookContainer.appendChild(m)
-        m.textContent = `Title: ${myLibrary[i].title} Author: ${myLibrary[i].author} Pages: ${myLibrary[i].pages} Read: ${myLibrary[i].read}`
-    }
-    
-    
+    console.log(myLibrary[myLibrary.length-1].title)
+    addNewest()
 }
 
 let button = document.getElementById('addbook')
@@ -39,3 +30,59 @@ button.addEventListener('click', () => {
     addBookToLibrary()
 })
 
+let refresh = document.querySelector('#refresh')
+
+function displayCard(){
+    
+
+newTitle.appendChild(document.createElement('div'))
+
+
+}
+
+for (const val of myLibrary){
+    let displayTitle = document.createElement('div')
+    displayTitle.classList.add('displayTitle')
+    newTitle.appendChild(displayTitle)
+    displayTitle.textContent = val.title
+    
+    let displayAuthor = document.createElement('div')
+    displayAuthor.classList.add('displayAuthor')
+    newAuthor.appendChild(displayAuthor)
+    displayAuthor.textContent = val.author
+
+    let displayPages = document.createElement('div')
+    displayPages.classList.add('displayPages')
+    newPages.appendChild(displayPages)
+    displayPages.textContent = val.pages
+
+    let displayRead = document.createElement('div')
+    displayRead.classList.add('displayRead')
+    newRead.appendChild(displayRead)
+    displayRead.textContent = val.read  
+}
+
+
+function addNewest(){
+    
+        let displayTitle = document.createElement('div')
+        displayTitle.classList.add('displayTitle')
+        newTitle.appendChild(displayTitle)
+        displayTitle.textContent = myLibrary[myLibrary.length-1].title
+        
+        let displayAuthor = document.createElement('div')
+        displayAuthor.classList.add('displayAuthor')
+        newAuthor.appendChild(displayAuthor)
+        displayAuthor.textContent = myLibrary[myLibrary.length-1].author
+    
+        let displayPages = document.createElement('div')
+        displayPages.classList.add('displayPages')
+        newPages.appendChild(displayPages)
+        displayPages.textContent = myLibrary[myLibrary.length-1].pages
+    
+        let displayRead = document.createElement('div')
+        displayRead.classList.add('displayRead')
+        newRead.appendChild(displayRead)
+        displayRead.textContent = myLibrary[myLibrary.length-1].read  
+    
+    }
